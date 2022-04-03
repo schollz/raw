d0ad0034f8afbb6e14e66be074a3a957a5105dbc
Zack Scholl <zack.scholl@gmail.com>
2022-04-02 18:55:18 -0700

    add sampswap test y

--- src/sampswap/sampswap.go
+++ src/sampswap/sampswap.go
@@ -1,6 +1,9 @@
 package sampswap
 
 import (
+	"math"
+	"math/rand"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -12,6 +15,7 @@ import (
 type SampSwap struct {
 	FileIn        string
 	FileOut       string
+	FileOriginal  string
 	TempoIn       float64
 	TempoOut      float64
 	TempoEstimate float64
@@ -54,15 +58,143 @@ func (ss *SampSwap) Run() (err error) {
 	}
 
 	// estimate bpm
+	if ss.TempoIn == 0 {
+		ss.TempoIn, err = estimateBPM(fname)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+	}
+	if ss.TempoOut == 0 {
+		ss.TempoOut = ss.TempoIn
+	}
 
+	// determine average number of beats
+	ss.BeatsIn = math.Floor(math.Round(sox.MustFloat(sox.Length(fname)) / (60 / ss.TempoIn)))
+	if ss.BeatsOut == 0 {
+		ss.BeatsOut = ss.BeatsIn
+	}
+
+	// add silence and truncate to the average number of beats
+	fname, err = sox.SilenceAppend(fname, 2)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	fname, err = sox.Trim(fname, 0, ss.BeatsIn*60/ss.TempoIn)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	// add repeats until we reach the number of wanted beats
+	ss.BeatsIn = math.Floor(math.Round(sox.MustFloat(sox.Length(fname)) / (60 / ss.TempoIn)))
+	for {
+		if ss.BeatsIn >= ss.BeatsOut {
+			break
+		}
+		fname, err = sox.Repeat(fname, 1)
+		if err != nil {
+			return
+		}
+		ss.BeatsIn = math.Floor(math.Round(sox.MustFloat(sox.Length(fname)) / (60 / ss.TempoIn)))
+	}
+
+	// trim off excess beats
+	log.Tracef("trimming to %fs", ss.BeatsOut*60/ss.TempoIn)
+	fname, err = sox.Trim(fname, 0, ss.BeatsOut*60/ss.TempoIn)
+	if err != nil {
+		return
+	}
+
+	for i := 0.0; i < ss.BeatsIn*ss.ProbPitch; i++ {
+		fname = ss.pitch(fname)
+	}
+	for i := 0.0; i < ss.BeatsIn*ss.ProbJump; i++ {
+		fname = ss.jump(fname)
+	}
+
+	err = os.Rename(fname, ss.FileOut)
 	return
 }
 
+func (ss *SampSwap) jump(fname string) (fname2 string) {
+	var err error
+	fname2 = fname
+	length_beat := randF(1, 5) / 2
+	start_beat := randF(1, ss.BeatsIn-length_beat)
+	paste_beat := randF(1, ss.BeatsIn-length_beat)
+	crossfade := 0.05
+	log.Tracef("jump - length %f start %f paste %f", length_beat, start_beat, paste_beat)
+	fname, err = sox.CopyPaste(fname, 60/ss.TempoIn*start_beat,
+		60/ss.TempoIn*(start_beat+length_beat),
+		60/ss.TempoIn*paste_beat,
+		crossfade)
+	if err == nil {
+		fname2 = fname
+	}
+	return
+}
+
+func (ss *SampSwap) pitch(fname string) (fname2 string) {
+	var err error
+	fname2 = fname
+	length_beat := randF(1, 4) / 8
+	start_beat := randF(1, ss.BeatsIn-length_beat*8)
+	paste_beat := start_beat
+	crossfade := 0.095
+	var piece string
+	piece, err = sox.Trim(ss.FileOriginal,
+		60/ss.TempoIn*start_beat-crossfade,
+		60/ss.TempoIn*length_beat+crossfade)
+	if err != nil {
+		return
+	}
+	piece, err = sox.Pitch(piece, rand.Intn(3)+1)
+	if err != nil {
+		return
+	}
+	fname, err = sox.Paste(fname, piece, 60/ss.TempoIn*paste_beat, crossfade)
+	if err == nil {
+		fname2 = fname
+	}
+	return
+}
+
+func randF(min, max float64) float64 {
+	return math.Round(min + rand.Float64()*(max-min))
+}
+
 func estimateBPM(fname string) (bpm float64, err error) {
+	// first see if the bpm appears in the filename
 	r, _ := regexp.Compile(`bpm(\d+)`)
 	bpm, err = strconv.ParseFloat(strings.TrimPrefix(r.FindString(fname), "bpm"), 64)
 	if err == nil {
 		return
 	}
+
+	// assume the file trimmed and guess it based on the length
+	audioLength, err := sox.Length(fname)
+	if err != nil {
+		return
+	}
+
+	// find the closest number of even measures
+	closestDiff := 1000000.0
+	for b := 100.0; b < 200; b++ {
+		measures := audioLength / ((60.0 / b) * 4)
+		measuresRounded := math.Round(measures)
+		if int(measuresRounded)%2 == 0 {
+			dif := math.Abs(measuresRounded - measures)
+			if int(measuresRounded)%8 == 0 {
+				dif = dif / 2
+			}
+			if dif < closestDiff {
+				closestDiff = dif
+				bpm = b
+			}
+		}
+	}
+
 	return
 }
--- src/sampswap/sampswap_test.go
+++ src/sampswap/sampswap_test.go
@@ -1,8 +1,10 @@
 package sampswap
 
 import (
+	"fmt"
 	"testing"
 
+	log "github.com/schollz/logger"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,4 +12,18 @@ func TestEstiamteTempo(t *testing.T) {
 	tempo, err := estimateBPM("this_is_4th_file_bpm_bpm123_45.wav")
 	assert.Nil(t, err)
 	assert.Equal(t, 123.0, tempo)
+	tempo, err = estimateBPM("175-16-104.wav")
+	assert.Nil(t, err)
+	assert.Equal(t, 175.0, tempo)
+}
+
+func TestRunApp(t *testing.T) {
+	log.SetLevel("trace")
+	ss := Init()
+	ss.FileIn = "175-16-104.wav"
+	ss.FileOut = "test.wav"
+	ss.ProbJump = 0.5
+	ss.BeatsOut = 16
+	assert.Nil(t, ss.Run())
+	fmt.Printf("ss: %+v\n", ss)
 }
